9cdf292a9655222d53ac4e1f9cd1ec5abb6e5159
Clerkly-online <johannes.plodeck@gmail.com>
2025-04-06 22:44:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,23 +10,9 @@ import Signup from "./pages/Signup_TMP";
 import Login from "./pages/Login_TMP";
 import Dashboard from "./pages/Dashboard_TMP";
 
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";
-
 function App() {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      setLoading(false);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
+  const [loading, setLoading] = useState(false); // false zum Testen
 
   return (
     <Router>
